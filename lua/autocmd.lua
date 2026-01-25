174d240743d12f0ae49b09196b0f65c245053ad7
LambBread <judahjohnson4702@gmail.com>
2026-01-25 12:22:11 -0800

    update config

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -14,3 +14,37 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = {"c", "cpp"},
     command = "setlocal filetype=cpp.doxygen"
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.cmd("NvimTreeToggle")
+        end
+    end,
+})
+
+-- turn numbers on only for normal file buffers
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  callback = function(args)
+    local bufnr = args.buf
+    local bt = vim.api.nvim_buf_get_option(bufnr, "buftype")
+    local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+    -- skip special buffers: terminals, NvimTree, help, etc.
+    local skip = {
+      terminal = true,
+      help = true,
+      qf = true,
+      NvimTree = true,
+      ["neo-tree"] = true,
+    }
+
+    if bt == "" and not skip[ft] then
+      vim.api.nvim_set_option_value("number", true, { scope = "local", win = 0 })
+      -- vim.api.nvim_set_option_value("relativenumber", true, { scope = "local", win = 0 })
+    else
+      vim.api.nvim_set_option_value("number", false, { scope = "local", win = 0 })
+      vim.api.nvim_set_option_value("relativenumber", false, { scope = "local", win = 0 })
+    end
+  end,
+})
--- lua/settings.lua
+++ lua/settings.lua
@@ -4,3 +4,5 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.number = false
+vim.opt.relativenumber = false
